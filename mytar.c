78cd3f5bbc14d2baae6aee9d8aa2d18d0cf8e81a
jcavalca <jcavalca@calpoly.com>
2020-11-08 12:52:12 -0800

    22/45 ... needs debugging

--- mytar.c
+++ mytar.c
@@ -11,41 +11,49 @@
 # include <fcntl.h>
 # include <grp.h>
 # include <arpa/inet.h>
-# include "special.c"
 # include <time.h>
 # include <ctype.h>
+# include <utime.h>
+
+# include "special.c"
 # include "print.c"
 # include "oct.c"
 # include "format.c"
 # include "write.c"
 # include "tree.c"
+# include "check.c"
+
 # define BLOCK_SIZE 512
 # define CAP_8 2097151
 # define CAP_12 8589934591
 
 
 
-/*This checks if we should list a file ...*/
-int file_check(char *file_name, int *flag_specific, 
-		int num_s, char **specific_file){
-
-	/*No specific files given ...*/
-	if (*flag_specific == 0)
-	return 1;	
-	/*Checking if file is in the given ones ...*/
-	else if (*flag_specific == 1){
+void get_name(char *file_name, uint8_t buf[BLOCK_SIZE]){
 	int count;
-	for (count = 0; count < num_s; count++){
-	if (strcmp(specific_file[count], file_name) == 0)
-	return 1;
-	}
-	}
-	/*Couldn't find it ...*/
-	return 0;
-
+ /*File name w/ no prefix ...*/
+        if (buf[345] == '\0'){
+        for (count = 0; count < 100; count++){
+        if (buf[count] == '\0')
+        break;
+        file_name[count] = buf[count];
+        }
+        /*W/ prefix ...*/
+        }else{
+        int stop = 0;
+        for (count = 0; buf[count + 345] != '\0'; count++){
+        stop++;
+        file_name[count] = buf[count + 345];}
+        for (count = 0; count < 100; count++)
+        if (count == 0 && buf[0] == '/')
+        continue;
+        else{
+        if (buf[count] == '\0')
+        break;
+        file_name[stop + count]  = buf[count];}
+        }
 
 }
-
 
 void zero_buf(uint8_t buf[BLOCK_SIZE]){
 	int count;
@@ -53,11 +61,192 @@ void zero_buf(uint8_t buf[BLOCK_SIZE]){
         buf[count] = 0;
 }
 
-/*For listind we need name (if verbose is on,
+void write_contents(char *file_name, int fd_tar, int size){
+
+	uint8_t buf[BLOCK_SIZE];
+	int dbs = size / BLOCK_SIZE; /*data blocks to be written ..*/
+	int read_ret, write_ret;
+	int count;
+	int fd_out;
+	if (size % BLOCK_SIZE > 0)
+	dbs = dbs + 1;
+	
+	zero_buf(buf);
+	
+	fd_out = open(file_name, O_RDWR);
+	if (fd_out < 0)
+	perror("open");
+
+	/*Archive is structured in blocks ...*/
+  	read_ret = read(fd_tar, &buf, BLOCK_SIZE);		
+	
+	if (-1 == read_ret){
+	perror("read");
+	return;}
+	
+	for (count = 0; count < dbs; count++){
+
+	if ((count == dbs - 1) && (size % BLOCK_SIZE > 0)){
+	write_ret = write(fd_out, &buf, size % BLOCK_SIZE);
+	break;}
+	else
+	write_ret = write(fd_out, &buf, read_ret);
+	
+	if (write_ret < 0)
+	perror("write");
+
+	read_ret = read(fd_tar, &buf, BLOCK_SIZE);
+	
+	}
+}
+
+/*For creating, we need size, type, name, permissions*/
+/*Restores mtime !*/
+void read_x(int fd_tar, int *flag_v, 
+                int *flag_specific, int numb_s, char **specific_files)
+{
+	uint8_t buf[BLOCK_SIZE];
+        int count;
+        char *file_name = calloc(200, 1);
+        char *size = calloc(12, 1);	
+	char *typeflag = calloc(1, 1);
+	char *mode = calloc(8, 1);
+	char *mtime = calloc(8, 1);
+	int read_ret, check, exec = 0, int_size = 0, mode_int = 0;
+	int open_ret;		
+	if (file_name == NULL || size == NULL || 
+		typeflag == NULL || mode == NULL ||
+		mtime == NULL){
+        perror("calloc");
+        exit(EXIT_FAILURE);}
+
+	if (-1 == lseek(fd_tar, SEEK_SET, 0))
+        perror("unable to read tar");
+	zero_buf(buf);
+
+	read_ret = read(fd_tar, &buf, BLOCK_SIZE);
+
+	if (read_ret < 0){
+        perror("read");
+        return;
+        }
+        while (buf[0] != '\0' || read_ret == 0) {
+
+       	get_name(file_name, buf);
+	*typeflag = buf[156];
+
+        check = file_check2(file_name, flag_specific, numb_s, specific_files);
+	int_size = get_size(size, buf);	
+
+	if (check == 1){
+	/*Mode ..*/
+        for (count = 100; count < 108; count++)
+        mode[count - 100] = buf[count];
+        mode_int = strtol(mode, NULL, 8);	
+
+	if (*flag_v == 1)
+	printf("%s\n", file_name);
+	
+	if (mode_int & S_IXUSR)
+	exec = 1;
+	if (mode_int & S_IXGRP)
+        exec = 1;
+	if (mode_int & S_IXOTH)
+        exec = 1;
+	/*If a regular file ...*/
+	if (*typeflag == '0' || *typeflag == '\0'){
+		if (exec == 1){
+		open_ret = open(file_name, O_CREAT | O_TRUNC  | O_APPEND, 
+			(S_IRUSR | S_IWUSR| S_IXUSR |
+			 S_IRGRP | S_IWGRP | S_IXGRP |
+			 S_IROTH | S_IWOTH | S_IXOTH) );	
+		}
+		else{
+		open_ret = open(file_name, O_CREAT | O_TRUNC | O_APPEND,
+                        (S_IRUSR | S_IWUSR| 
+                         S_IRGRP | S_IWGRP |
+			 S_IROTH | S_IWOTH));
+		}
+	}
+	/*If a symlink ...*/
+	else if (*typeflag == '2'){
+		char *linkname = calloc(100, 1);
+		
+		for (count = 157; count < 257; count++)
+		linkname[count - 157] = buf[count];
+
+		open_ret = symlink(linkname ,file_name);
+
+		free(linkname);
+	}
+	/*If a directory ...*/
+	else if (*typeflag == '5'){	
+		open_ret = mkdir(file_name, O_RDWR);		
+	}
+	if (open_ret < 0)
+	printf("unable to extract %s\n", file_name);
+	else{
+	if (*typeflag != '0' && *typeflag != '\0')
+	close(open_ret);}
+	if (open_ret > 0 && check == 1 &&  
+		(*typeflag == '0' || *typeflag == '\0'))
+	write_contents(file_name, fd_tar, int_size);
+
+	/*Restoring mtime ...*/
+	if (open_ret >= 0){
+	struct utimbuf time;
+	int val = get_mtime(mtime, buf);
+
+	if (localtime(&time.actime) == NULL)
+	perror("time");
+	time.modtime = val;
+	if ( -1 == utime(file_name, &time))
+	perror("utime");
+	}
+		
+	
+	if (-1 == close(open_ret))
+	perror("close");
+	read_ret = read(fd_tar, &buf, BLOCK_SIZE);
+        if (read_ret < 0)
+        perror("read");
+	}
+	/*Not interested in contents we are not extracting ...*/
+	if (check != 1) {
+	int jump, dbs;
+
+	dbs = int_size / BLOCK_SIZE;
+        if (int_size % BLOCK_SIZE != 0)
+        dbs = dbs + 1;
+        jump = dbs * BLOCK_SIZE;
+        if (dbs != 0){
+        if (-1 == lseek(fd_tar, jump , SEEK_CUR))
+        perror("lseek");
+        }
+
+        read_ret = read(fd_tar, &buf, BLOCK_SIZE);
+        if (read_ret < 0){
+        perror("read");
+        return;
+	}
+	
+	}
+	}
+	free(file_name);
+	free(mode);
+	free(size);
+	free(mtime);
+	free(typeflag);		
+}
+
+
+
+
+/*For listing we need name (if verbose is on,
  * 				 we also need size, perm, 
  * 				 user/grp and mtime)
 */
-void read_list(int fd_tar, int *flag_v, int *flag_t, 
+void read_t(int fd_tar, int *flag_v, int *flag_t, 
 		int *flag_specific, int numb_s, char **specific_files)
 {
 	uint8_t buf[BLOCK_SIZE];
@@ -72,7 +261,6 @@ void read_list(int fd_tar, int *flag_v, int *flag_t,
 	int read_ret;
 	int jump;
 	int check = 0;
-	char linkname[100];
 	typeflag = calloc(1,1);
 	zero_buf(buf);
 
@@ -84,7 +272,10 @@ void read_list(int fd_tar, int *flag_v, int *flag_t,
 	perror("unable to read tar");
 	
 	read_ret = read(fd_tar, &buf, BLOCK_SIZE);
-
+	if (read_ret < 0){
+	perror("read");
+	return;
+	}
 	while (buf[0] != '\0' || read_ret == 0)	{
 
 	/*File name w/ no prefix ...*/	
@@ -110,7 +301,7 @@ void read_list(int fd_tar, int *flag_v, int *flag_t,
         }
 	*typeflag = buf[156];
 	
-	check = file_check(file_name, flag_specific, numb_s, specific_files);
+	check = file_check2(file_name, flag_specific, numb_s, specific_files);
 		
 	if (*flag_v == 1 && *flag_t == 1){
 	uname = calloc(8, 1);
@@ -143,6 +334,7 @@ void read_list(int fd_tar, int *flag_v, int *flag_t,
 	if (check == 1)
 	print_mtime(mtime, buf);
 	}
+
 	int_size = get_size(size, buf);
 	if (check == 1){
 	printf("%s \n", file_name);
@@ -158,8 +350,21 @@ void read_list(int fd_tar, int *flag_v, int *flag_t,
 	}
 
 	read_ret = read(fd_tar, &buf, BLOCK_SIZE);
+	if (read_ret < 0){
+        perror("read");
+        return;
+        }
 	}
-	 free(file_name);
+	 if (*flag_v == 1 && *flag_t == 1){
+	free(uname);
+	free(gname);
+	free(mtime);
+	free(mode);
+
+	}
+	free(size);
+	free(typeflag);
+	free(file_name);
 }
 
 
@@ -188,36 +393,6 @@ void command_parser( int *flag_c, int *flag_t, int *flag_x, int *flag_v,
 
 	}
 }
-
-void check_tar(char *file_tar, int fd_tar){
-	struct stat buf;
-	char read_buf[BLOCK_SIZE];
-	if (-1 == lstat(file_tar, &buf)){
-	perror("lstat");
-	exit(EXIT_FAILURE);
-	}
-
-	if (-1 == read(fd_tar, read_buf, BLOCK_SIZE)){
-	perror("read");
-	 exit(EXIT_FAILURE);
-	}		
-
-	if (S_ISREG(buf.st_mode) || 	 /*tar should be reg. files*/
-	buf.st_size % BLOCK_SIZE == 0 || /*tar should have integer # blocks*/
-	buf.st_size != 0 ||		 /*tar should be non=empty*/
-	isalpha(read_buf[0]) != 0)
-	return;
-	else{
-	perror("malformed tar file");
-        exit(EXIT_FAILURE);
-	}
-	if (-1 == lseek(fd_tar, 0, SEEK_SET)){
-        perror("lstat");
-        exit(EXIT_FAILURE);
-        }
-
-}
-
 
 
 int main(int argc, char *argv[]){
@@ -302,7 +477,7 @@ int main(int argc, char *argv[]){
 	}
 	
 
-	else if(*flag_t == 1){
+	else if(*flag_t == 1 || *flag_x == 1){
 	char **specific_files;	
 	/*If listing specific files ...*/
 	if (argc>3){
@@ -318,9 +493,13 @@ int main(int argc, char *argv[]){
 	exit(EXIT_FAILURE);
 	}
 	
-	check_tar(argv[2], fd_tar);
-	read_list(fd_tar, flag_v, flag_t, flag_specific, 
+	check_tar2(argv[2], fd_tar);
+	if (*flag_t == 1)
+	read_t(fd_tar, flag_v, flag_t, flag_specific, 
 		argc - 3,  specific_files);
+	else
+	read_x(fd_tar, flag_v, flag_specific,
+                argc - 3,  specific_files);
 	}
 
 
